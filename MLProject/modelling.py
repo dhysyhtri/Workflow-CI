6e7c83642aeddd8375f81064f15e57ea96d4de59
Dheysavina Dwi Syahputri <dhy.syhtri@gmail.com>
2025-12-24 05:59:34 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,13 +1,18 @@
 import os
 import pandas as pd
+import mlflow
+import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-import joblib
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Load data
+# MLflow tracking DagsHub
+mlflow.set_tracking_uri("https://dagshub.com/dhysyhtri/amazon-sales-mlflow.mlflow")
+mlflow.set_experiment("amazon-sales-ci")
+
+# Load dataset
 DATA_PATH = os.path.join(BASE_DIR, "amazon_sales_clean.csv")
 data = pd.read_csv(DATA_PATH)
 
@@ -16,16 +21,18 @@ y = (data["label_profit"] > 0).astype(int)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train model
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+with mlflow.start_run() as run:
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
 
-# Evaluate
-acc = accuracy_score(y_test, model.predict(X_test))
-print(f"Training selesai | Accuracy: {acc}")
+    acc = accuracy_score(y_test, model.predict(X_test))
+    print(f"Training selesai | Accuracy: {acc}")
 
-# Save model sebagai file .pkl
-MODEL_PATH = os.path.join(BASE_DIR, "model.pkl")
-joblib.dump(model, MODEL_PATH)
-print(f"Model tersimpan di {MODEL_PATH}")
+    # Log metric & parameter
+    mlflow.log_param("model_type", "RandomForest")
+    mlflow.log_metric("accuracy", acc)
 
+    # Log model ke MLflow (artifact_path='model')
+    mlflow.sklearn.log_model(model, artifact_path="model")
+
+    print(f"RUN_ID: {run.info.run_id}")
